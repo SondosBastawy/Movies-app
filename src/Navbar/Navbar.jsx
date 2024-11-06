0185de79321077468b4d06be7a94ee58e27ea559
Sondos Mahmoud <sondosbastawy3@gmail.com>
2024-11-06 15:10:25 +0200

    delete input and final commit

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -3,25 +3,11 @@ import { FaSearch } from 'react-icons/fa'
 import { NavLink  } from 'react-router-dom'
 
 export default function Navbar(props) {
-
-  const [input, setInput] = useState("")
-  const handleChange = (value)=>{
-  setInput(value)
-  
-}
-  const toggleActive = (e) => {
-    console.log(e);
-  }
-
   return (
     <>
   <nav className="navbar navbar-expand-lg bg-success roboto-slab-font">
   <div className="container-fluid">
     <NavLink className="navbar-brand px-2" to ="#"><b>Movies & Tv Shows</b></NavLink>
-    <div className='input-search mx-5'>
-    <FaSearch id="search-icon" />
-    <input type="text" placeholder= ' Type to search...' value={input} onChange={(e) => handleChange(e.target.value)}/>
-    </div>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0 px-5">
         <li className="nav-item">
